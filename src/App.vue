e51b302796759db656e415d4caee924b2738f6af
Zhao boZhen <2661469338@qq.com>
2023-04-27 15:44:31 +0800

    message-model提交

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+  import { MyMessageBox } from './MyUI';
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  const showMessageBox = () => {
+    MyMessageBox({
+      confirmBtnText: '确定',
+      title: 'MessageBox',
+      content: 'This is MessageBox content.'
+    })
+    .then(() => {
+      console.log('MessageBox resolve');
+    }).catch(() => {
+      console.log('MessageBox reject');
+    });
   }
-}
+
+  const showConfirmMessageBox = () => {
+    MyMessageBox.confirm({
+      showCancelBtn: true,
+      cancelBtnText: '取消',
+      title: 'ConfirmMessageBox',
+      content: 'This is ConfirmMessageBox content.'
+    })
+    .then(() => {
+      console.log('ConfirmMessageBox resolve');
+    }).catch(() => {
+      console.log('ConfirmMessageBox reject');
+    });
+  }
+
+  const showPromptMessageBox = () => {
+    MyMessageBox.prompt({
+      confirmBtnText: '确定',
+      showCancelBtn: true,
+      cancelBtnText: '取消',
+      title: 'PromptMessageBox',
+      content: 'This is PromptMessageBox content.'
+    })
+    .then((value) => {
+      console.log('PromptMessageBox resolve:', value);
+    }).catch(() => {
+      console.log('PromptMessageBox reject');
+    });
+  }
 </script>
 
+<template>
+  <div>
+    <button @click="showMessageBox">showMessageBox</button>
+    <button @click="showConfirmMessageBox">showConfirmMessageBox</button>
+    <button @click="showPromptMessageBox">showPromptMessageBox</button>
+  </div>
+</template>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/MyUI/index.js
@@ -0,0 +1,5 @@
+import MyMessageBox from './MessageBox';
+
+export {
+  MyMessageBox
+}
